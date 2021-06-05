4f7f30a2a1f29773725593cb1be7a8e63fac797a
Janis Lebron <janislebron@gmail.com>
2021-06-05 12:09:40 +0000

    Automatically backed up by Learn

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,9 +1,8 @@
-class Team 
+class Team
   attr_accessor :name, :motto
   
-  def initialize(args)
-    @name = args[:name]
-    @motto = args[:motto]
+  def initialize(params)
+    @name = params[:name]
+    @motto = params[:motto]
   end
-
-end 
+end
