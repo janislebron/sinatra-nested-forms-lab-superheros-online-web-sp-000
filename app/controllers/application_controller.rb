2de1180d66c465f63862b06f593d2f67e1fd1f47
Janis Lebron <janislebron@gmail.com>
2021-06-05 12:07:25 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,20 @@
+  
 require 'sinatra/base'
 
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
-
+  get '/' do
+    erb :super_hero
+  end
+  
+  post '/team' do
+      @team = Team.new(params[:team])
+      members = params[:team][:members]
+      @super_heros = members.collect do |member_params|
+        SuperHero.new(member_params)
+      end
+      erb :team
+    end
 end
